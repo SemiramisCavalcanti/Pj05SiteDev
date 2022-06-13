02374b6676f39191ab9f525cf236ce6b35502a06
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-06-13 13:36:01 -0300

    inclusao de header e footer

---
+++ paginas/include/footer.php
@@ -0,0 +1,36 @@
+<!-- Fim Corpo Site -->
+
+<!-- Footer -->
+
+<footer class="mt-5 rounded">
+<div class="row">
+    <div class="col">
+        <img src="img\png-transparent-computer-security-threat-information-sitelock-grease-miscellaneous-text-trademark.png" alt="" width="150" class="ms-5 mt-4">
+        
+    </div>
+    <div class="col">
+        <img src="img\FenixEchoRoxa.png" alt="" width="120" class="ms-5 p-2">
+
+    </div>
+    <div class="col mt-5 me-2 letramaisescura">
+    <p><i class="fas fa-copyright"></i> Todos os direitos reservados FenixEcho.com.br</p>
+    </div>
+</div>
+
+
+
+</footer>
+
+
+<!-- Fim Footer -->
+
+
+<!-- Lnks JS -->
+<script src="https://ajax.googleapis.com/ajax/"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="plugins\bootstrap-5.1.3-dist\bootstrap-5.1.3-dist\js\bootstrap.bundle.js"></script>
+<script src="plugins/js/jquery-3.5.1.min.js"></script>
+
+</body>
+
+</html>
