7aeab77042027e138f55aae470cae091777a24eb
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-06-13 14:19:38 -0300

    ajsutes topo de pagina

--- paginas/inicial.php
+++ paginas/inicial.php
@@ -2,10 +2,7 @@
     <div class="container">
         <div class="row mt-3 text-light">
             <div class="col">
-                <h4>Portifólio</h4>
-            </div>
-            <div class="col">
-                <h5>Semíramis Cavalcanti <small> - Fenix Echo - </small> <img src="img/FenixEchoBranca.png" alt="" width="50"></h5>
+                <h5> <small class="fs-3"> Portifólio | </small> Semíramis Cavalcanti <small class="fs-3"> | </small> <img src="img/FenixEchoBranca.png" alt="" width="50"></h5>
             </div>
         </div>
     </div>
