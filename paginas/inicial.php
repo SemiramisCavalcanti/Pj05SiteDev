05f34b06e5ba91f004cbf85eb663d28cd419abf3
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-06-15 08:44:18 -0300

    links linkedin, github e instagram

--- paginas/inicial.php
+++ paginas/inicial.php
@@ -9,8 +9,10 @@
 </div>
 <!-- https://via.placeholder.com/1400x450 -->
 <img src="img/QuemSou01.png" alt="" class="mt-2">
-<div class="container">
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
+<div class="topo">
+    <div class="container text-center fs-3 p-1">
+        <a href="https://www.linkedin.com/in/semiramis-cavalcanti-01a18013b/" class="link-light text-decoration-none me-3"><i class="fab fa-github"></i> </a>
+        <a href="https://www.linkedin.com/in/semiramis-cavalcanti-01a18013b/" class="link-light text-decoration-none  ms-3 me-3"> <i class="fab fa-linkedin"></i> </a>
+        <a href="https://www.linkedin.com/in/semiramis-cavalcanti-01a18013b/" class="link-light text-decoration-none ms-3"> <i class="fab fa-instagram"></i> </a>
+    </div>
 </div>
