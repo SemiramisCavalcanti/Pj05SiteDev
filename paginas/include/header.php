c720ecf9589b28de0180102c3b0b4849c03d034f
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-06-15 14:02:21 -0300

    ajuste de cursos e certificados ; header e footer

--- paginas/include/header.php
+++ paginas/include/header.php
@@ -5,14 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    
+
     <title>Portifólio</title>
 
     <link rel="stylesheet" href="plugins\bootstrap-5.1.3-dist\bootstrap-5.1.3-dist\css\bootstrap.min.css">
     <link rel="stylesheet" href="plugins\fontawesome-free\css\all.min.css">
     <link rel="stylesheet" href="css/style.css">
-    
-    
+
+
 </head>
+<header>
+        <div class="container">
+            <div class="row text-light">
+                <div class="col">
+                    <h5 class="mt-3"> <small class="fs-3"> Portifólio | </small> Semíramis Cavalcanti <small class="fs-3"> | </small> <img src="img/FenixEchoBranca.png" alt="" width="50"></h5>
+                </div>
+            </div>
+        </div>
+</header>
 
 <body class="">
--- paginas/inicial.php
+++ paginas/inicial.php
@@ -1,12 +1,4 @@
-<div class="fixed-top topo">
-    <div class="container">
-        <div class="row mt-3 text-light">
-            <div class="col">
-                <h5> <small class="fs-3"> Portifólio | </small> Semíramis Cavalcanti <small class="fs-3"> | </small> <img src="img/FenixEchoBranca.png" alt="" width="50"></h5>
-            </div>
-        </div>
-    </div>
-</div>
+
 <!-- https://via.placeholder.com/1400x450 -->
 <img src="img/QuemSou01.png" alt="" class="mt-2">
 <div class="topo">
@@ -16,13 +8,77 @@
         <a href="https://www.linkedin.com/in/semiramis-cavalcanti-01a18013b/" class="link-light text-decoration-none ms-3"> <i class="fab fa-instagram"></i> </a>
     </div>
 </div>
-<div class="container letraescura mt-5"> <h2>Cursos e Certificados</h2> </div>
-
-<div class="row">
-    <div class="col text-center" width="300">
-        <!-- Curso --> <h4></h4>
-        <!-- Instituição --> <h5></h5>
-        <!-- Professor --> <h5></h5>
-        <img src="" alt="" width="250">
+<div class="container letraescura mt-1">
+    <div class="col-md">
+    <h2>Cursos e Certificados</h2>
+        <div class="row rounded cursosEcertificados">
+            <div class="col-4">
+                <div class="itens p-1 rounded mt-3">
+                    <a href="#4" class="text-decoration-none text-light">Certificado de Autoridade HTML <small>| DevMedia</small></a> <br>
+                </div>
+                <div class="itens p-1 rounded mt-3">
+                    <a href="#3" class="text-decoration-none text-light">Primeiros passos para Desenvolvimento Web <small> | Digital Innovation One Inc. (6h)</small></a> <br>               
+                </div>
+                <div class="itens p-1 rounded mt-3">
+                    <a href="#2" class="text-decoration-none text-light">Programador Web <small> | SENAC PE (240h)</small></a> <br>   
+                </div>
+                <div class="itens p-1 rounded mt-3">
+                    <a href="#1" class=" text-light text-decoration-none">Web Designer <small> | SENAC PE (160h)</small></a> <br>    
+                </div>
+               
+            </div>
+            <div class="col-8 certificados text-center">
+                <div class="" id="4">
+                    <img src="img\devmediaHtml.png" alt="" width="650" class="p-4">
+                </div>
+                <div class="" id="3">
+                    <img src="img\Dio01.png" alt="" width="650" class="p-4">
+                </div>
+                <div class="" id="2">
+                    <img src="img\programadorWeb.png" alt="" width="650" class="p-4">
+                </div>
+                <div class="" id="1">
+                    <img src="img\webDesigner.png" alt="" width="650" class="p-4">
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+
+<div class="row mt-5 ">
+    <div class="col text-center">
+        <img src="img\devmediaHtml.png" alt="" width="600" class="certificados p-4">
+    </div>
+    <div class="col text-center">
+        <img src="img\programadorWeb.png" alt="" width="600" class="certificados p-4">
+    </div>
+</div>
+<div class="row mt-3 ">
+    <div class="col text-center">
+        <img src="img\webDesigner.png" alt="" width="600" class="certificados p-4">
+    </div>
+    <div class="col text-center">
+        <img src="img\programadorWeb.png" alt="" width="600" class="certificados p-4">
+    </div>
+</div> -->
