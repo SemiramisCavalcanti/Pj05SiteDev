ca74181589f0fd287bea9d89fbb6a7954b404b46
Semiramis Cavalcanti <webdesigncavalcanti@gmail.com>
2022-06-15 14:41:11 -0300

    cards projetos

--- paginas/inicial.php
+++ paginas/inicial.php
@@ -1,6 +1,5 @@
-
-<!-- https://via.placeholder.com/1400x450 -->
 <img src="img/QuemSou01.png" alt="" class="mt-2">
+
 <div class="topo">
     <div class="container text-center fs-3 p-1">
         <a href="https://www.linkedin.com/in/semiramis-cavalcanti-01a18013b/" class="link-light text-decoration-none me-3"><i class="fab fa-github"></i> </a>
@@ -8,24 +7,24 @@
         <a href="https://www.linkedin.com/in/semiramis-cavalcanti-01a18013b/" class="link-light text-decoration-none ms-3"> <i class="fab fa-instagram"></i> </a>
     </div>
 </div>
-<div class="container letraescura mt-1">
+<div class="container letraescura mt-5">
     <div class="col-md">
-    <h2>Cursos e Certificados</h2>
+        <h2>Cursos e Certificados</h2>
         <div class="row rounded cursosEcertificados">
             <div class="col-4">
                 <div class="itens p-1 rounded mt-3">
                     <a href="#4" class="text-decoration-none text-light">Certificado de Autoridade HTML <small>| DevMedia</small></a> <br>
                 </div>
                 <div class="itens p-1 rounded mt-3">
-                    <a href="#3" class="text-decoration-none text-light">Primeiros passos para Desenvolvimento Web <small> | Digital Innovation One Inc. (6h)</small></a> <br>               
+                    <a href="#3" class="text-decoration-none text-light">Primeiros passos para Desenvolvimento Web <small> | Digital Innovation One Inc. (6h)</small></a> <br>
                 </div>
                 <div class="itens p-1 rounded mt-3">
-                    <a href="#2" class="text-decoration-none text-light">Programador Web <small> | SENAC PE (240h)</small></a> <br>   
+                    <a href="#2" class="text-decoration-none text-light">Programador Web <small> | SENAC PE (240h)</small></a> <br>
                 </div>
                 <div class="itens p-1 rounded mt-3">
-                    <a href="#1" class=" text-light text-decoration-none">Web Designer <small> | SENAC PE (160h)</small></a> <br>    
+                    <a href="#1" class=" text-light text-decoration-none">Web Designer <small> | SENAC PE (160h)</small></a> <br>
                 </div>
-               
+
             </div>
             <div class="col-8 certificados text-center">
                 <div class="" id="4">
@@ -45,40 +44,38 @@
     </div>
 </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- 
-
-<div class="row mt-5 ">
-    <div class="col text-center">
-        <img src="img\devmediaHtml.png" alt="" width="600" class="certificados p-4">
+<div class="container mt-5">
+    <h2>Projetos</h2>
+    <div class="row text-light text-center m-auto">
+        <div class="col mt-3">
+            <div class="card topo" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Blog Estudantes</h5>
+                    <p class="card-text">Projeto desenvolvido.</p>
+                    <a href="#" class="btn btn-outline-light">Go somewhere</a>
+                </div>
+            </div>
+        </div>  
+        <div class="col mt-3">
+            <div class="card topo" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-light">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col mt-3">
+            <div class="card topo" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-light">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+       
     </div>
-    <div class="col text-center">
-        <img src="img\programadorWeb.png" alt="" width="600" class="certificados p-4">
-    </div>
+
+
 </div>
-<div class="row mt-3 ">
-    <div class="col text-center">
-        <img src="img\webDesigner.png" alt="" width="600" class="certificados p-4">
-    </div>
-    <div class="col text-center">
-        <img src="img\programadorWeb.png" alt="" width="600" class="certificados p-4">
-    </div>
-</div> -->
